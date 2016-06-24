5450fcf72d49013ecedf4c4c9ea258d5e5104ef3
jw <jw@demidi.eu>
2016-06-24 16:57:17 -0500

    fix reading works

---
+++ ecef.rb
@@ -0,0 +1,112 @@
+##!/usr/bin/env ruby
+#
+# Convert ECEF to LLA and back
+
+require 'geoutm'
+
+class ECEF
+	attr_accessor :x, :y, :z
+	# Ellipsoid parameters
+	WGS84 = { 	
+		:a => 6378137.0,				# semi-major axis
+		:f => 1.0/298.257223563,
+		:b => 6356752.314245179,
+		:ecc => 0.081819218048345,
+		:e2 => 6.6943799901377997e-3, 	# first eccentric (2f-f**2)
+	}
+	RAD2DEG = 180.0/Math::PI
+	DEG2RAD = Math::PI/180.0
+
+
+	def initialize(x, y, z)
+		@x 	= x.to_f
+		@y	= y.to_f
+		@z	= z.to_f
+	end
+
+	def ECEF::at(coordinates) 
+		case coordinates
+		when ECEF
+			self.new(coordinates.x, coordinates.y, coordinates.z)
+		when GeoUtm::LatLon
+			self.at(self.lla2xyz(coordinates.lat, coordinates.lon))
+		when GeoUtm::UTM
+			latlon = coordinates.to_lat_lon
+			self.at(latlon)
+		else
+			raise "Cannot initialize #{self} with #{coordinates} 
+				of type #{coordinates.class}"
+		end
+	end
+
+
+	def ECEF::lla2xyz(latitude, longitude, altitude = 0.0)
+		lat = latitude.to_f * DEG2RAD
+		lon = longitude.to_f * DEG2RAD
+		alt = altitude.to_f
+
+		a = ECEF::WGS84[:a]
+		f = ECEF::WGS84[:f]
+		e2 = ECEF::WGS84[:e2]
+		
+		chi = Math.sqrt( 1.0 - e2*(Math.sin(lat))**2 )
+		x = ( a/chi  + alt )*Math.cos(lat)*Math.cos(lon)
+		y = ( a/chi  + alt )*Math.cos(lat)*Math.sin(lon)
+		z = ( a*(1.0 - e2)/chi + alt )*Math.sin(lat)
+		return ECEF.new(x, y, z)
+	end
+
+#	def utm2xyz(utm_n, utm_e, zone, altitude = 0.0)
+#		zoneString = zone.to_s
+#		n = utm_n.to_f
+#		e = utm_e.to_f
+#		lonlat = GeoUtm::UTM.new(n, e, zoneString)
+#		
+#		
+#	end
+
+
+	def lon
+		lon_rad = Math.atan2(@y, @x)
+		return lon_rad * RAD2DEG
+	end
+
+	def lat
+		a = ECEF::WGS84[:a]
+		b = ECEF::WGS84[:b]
+		ecc = ECEF::WGS84[:ecc]
+		p = Math.sqrt(x*x + y*y)
+		theta = Math.atan(z*a/(p*b) )
+		sint3 = Math.sin(theta)*Math.sin(theta)*Math.sin(theta)
+		cost3 = Math.cos(theta)*Math.cos(theta)*Math.cos(theta)
+
+		numlat = z + ((a*a - b*b)/b)*sint3
+		denlat = p - ecc*ecc*a*cost3
+		lat_rad =  Math.atan( numlat/denlat )
+		return lat_rad * RAD2DEG
+	end
+
+	def alt
+		a = ECEF::WGS84[:a]
+		ecc = ECEF::WGS84[:ecc]
+		lat = self.lat * DEG2RAD
+		p = Math.sqrt(x*x + y*y)
+		ntemp = 1.0 - (ecc*Math.sin(lat))**2
+		if ntemp < 0.0
+			n = a
+		else
+			n = a / Math.sqrt( ntemp )
+		end
+		alt = (p / Math.cos(lat)) - n
+		return alt
+	end
+
+	def to_LatLon
+		 return GeoUtm::LatLon.new(self.lat, self.lon)
+	end
+
+	def to_utm
+		return GeoUtm::LatLon.new(self.lat, self.lon).to_utm
+	end
+end
+
--- gdf.rb
+++ gdf.rb
@@ -1,34 +1,20 @@
-#!/usr/bin/env ruby
+#!/usr/bin/env ruby/
 #
 
 require 'bindata'
+require 'geoutm'
+require_relative 'ecef'
 
 module GDF
 	VALID_POSTFIX = "\x01\x03\x03"
 	T0 = Time.utc(2000, 01, 01, 0, 0, 0)
 
-	# Define the records within a GDF: 
+	
 
-	# Header
-	# This is the first portion with gdf metadata
-	# It occurs once per file, at the beginning
-	class Header < BinData::Record
-		endian :little
-		uint32 :collar_id
-		string :prefix, :read_length => 4
-		string :msg, :read_length => 16
-		string :postfix, :read_length => 3
-		virtual :valid, :assert =>	lambda { postfix == GDF::VALID_POSTFIX }
-	end
 
-	# Junk
-	# This follow the header, and appears to 
-	# have no bearing on the data.
-	# Not sure of its purpose
-	class Junk < BinData::Record
-		string :raw, :read_length => 51
-	end
-
+	
+	# Records within each Fix: ###################
+	
 	# Datetime
 	# DateTime stored as an integer
 	# of seconds since 20010101_00:00:00
@@ -50,43 +36,195 @@ module GDF
 	# Coord
 	# This is a subsection with xyz ecef coordinates
 	# It is used within the larger Fix Record
-	class Coord < BinData::Record
+	class Coord < BinData::Primitive
 		endian :big
 		int32 :ecef_x
 		int32 :ecef_y
 		int32 :ecef_z
+		
+		def get
+			return ECEF.new(ecef_x, ecef_y, ecef_z)
+		end
+
+		def set(ecef_in)
+			self.ecef_x = ecef_in.x.to_i
+			self.ecef_y = ecef_in.y.to_i
+			self.ecef_z = ecef_in.z.to_i
+		end
 	end
 
-	# Sats
-	# This is a subsection of a fix
-	# It contains the data on satellites used
-	# and signal strength
-	class Sats < BinData::Record
+	class MortStatus < BinData::Primitive
+		bit3 :mort_idx
 
+		def get
+			return MORT_STATUS_VALUES[self.mort_idx]
+		end
 
-		endian	:little
+		def set(mort_in)
+			case mort_in
+			when Fixnum
+				self.mort_idx = mort_in.to_i % 0x7
+			when String
+				self.mort_idx = MORT_STATUS_VALUES.index(mort_in)
+			else
+				raise "Cannot set MortStatus with #{mort_in.class} object"
+			end
+		end
+
+	end
+	
+
+	class FixType < BinData::Primitive
+		bit5 :fix_idx
+
+		def get 
+			return FIX_TYPES[self.fix_idx]
+		end
+
+		def set(fix_in)
+			case fix_in
+			when Fixnum
+				self.fix_idx = fix_in.to_i % 256
+			when String
+				self.fix_idx = FIX_TYPES.index(fix_in)
+			else
+				raise "Cannot set FixType with #{fix_in.class} object"
+			end
+		end
 	end
 
+	class DOP < BinData::Primitive
+		endian :big
+		uint8 :dop_byte
+
+		def get
+			return dop_byte.to_f / 5.0
+		end
+		
+		def set(dop_in)
+			self.dop_byte = (dop_in * 5).round % 256
+		end
+	end
+
+	class Sat < BinData::Record
+		bit5 :num
+		bit3 :snr_raw
+		virtual :snr, :value => lambda {
+			num.nonzero?? snr_raw * 3 + 29 : 0
+		}
+	end
+
+	class SatArray < BinData::Array
+		default_parameters :type => :sat,
+			:initial_length => 11
+	end
+
+	class Error < BinData::Primitive
+		endian :big
+		uint8  :error_raw
+
+		def get
+			if (0x01..0xFE) === self.error_raw 
+				self.error_raw.to_f / 5.0
+			else
+				return NA
+			end
+		end
+
+		def set(err = 0.0)
+			self.error_raw = (err * 5.0).to_i % 256
+		end
+	end
+
+
+
+	class Voltage < BinData::Primitive
+		endian :big
+		uint8 :v_byte
+
+		def get
+			return v_byte.to_f / 50.0
+		end
+
+		def set(v_in)
+			self.v_byte = (v_in * 50).round % 256
+		end
+	end
+
+	class Temperature < BinData::Primitive
+		endian :big
+		uint8 :temp_byte
+
+		def get
+			return temp_byte.to_i - 103
+		end
+
+		def set(temp_in)
+			self.temp_byte = (temp_in + 103).to_i % 256
+		end
+	end
+
+
+	# Header
+	# This is the first portion with gdf metadata
+	# It occurs once per file, at the beginning
+	class Header < BinData::Record
+		endian :little
+		uint32 :collar_id
+		string :prefix, :read_length => 4
+		string :msg, :read_length => 16
+		string :postfix, :read_length => 3
+		virtual :valid, :assert =>	lambda { postfix == GDF::VALID_POSTFIX }
+	end
+
+	# Junk
+	# This follow the header, and appears to 
+	# have no bearing on the data.
+	# Not sure of its purpose
+	class Junk < BinData::Record
+		string :raw, :read_length => 51
+	end
 
 	# Fix 
 	# A single gps fix, with all associated data.
 	# After the Header and Junk, the rest of the gdf
 	# is just repeated fixes.
 	class Fix < BinData::Record
-		endian 	:little
-		uint32 	:time_seconds
-		coord  	:coord
-		uint8 	:fix_type
-		uint8	:dop
+		default_parameter :id => nil
 
-		uint8	:main_v
-		uint8	:bkp_v
-		uint8	:temp
+		virtual :collarID, :value => :id
+
+		time_record :time
+		coord 		:position
+		mort_status :mortality
+		fix_type 	:fixType
+		dop			:degreeOfPrecision
+		sat_array 	:sats
+		error		:error3D
+		voltage		:mainV
+		voltage		:beaconV
+		temperature :tempC
+
 	end
 
 
 
-	fix_types = ['No Fix',
+
+	MORT_STATUS_VALUES = 
+	[	'N/A',
+		'normal',
+		'Low Activity no radius',
+		'Low Activity within radius',
+		'Low Activity out of radius',
+		'Mortality no radius',
+		'Mortality within radius',
+		'Mortality out of radius'
+	]
+
+
+
+	FIX_TYPES = 
+		['No Fix',
 		'GPS-1 Sat',
 		'GPS-2 Sat',
 		'GPS-2D',
